81ca627829b922448de2c7d56f5f157bc3637145
brian <kipkoechbrian0@gmail.com>
2022-09-26 13:25:06 +0300

    complete assignment

--- src/randomColorGenerator.js
+++ src/randomColorGenerator.js
@@ -1,10 +1,10 @@
 // Nothing in this file needs to be altered (but it is your solution so feel free to!)
 export function getRandomColor() {
   // this function generates a random hex color from the letters below
-  const letters = "123456789AB"; // <-- cutting off top end so we get lighter colors
+  const letters = "123456789ABCDEF"; // <-- cutting off top end so we get lighter colors
   let color = "#";
-  for (let i = 0; i < 3; i++) {
-    color += letters[Math.floor(Math.random() * 11)];
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 15)];
   }
   return color;
 }
